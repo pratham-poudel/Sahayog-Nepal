db982b93b5068d8b5f7bdd79c26688505f50dc4e
prathampoudel <prathampoudel2@gmail.com>
2025-10-06 22:06:11 +0545

    Dist location changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(import.meta.dirname, "..", "dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
